c1f6aca1f03970af50a01475c373a5c40cfbe7d5
HamidExperience <Hamiddolanan@gmail.com>
2026-07-02 20:33:11 +0700

    Add user mode selection

---
+++ main.py
@@ -0,0 +1,17 @@
+# Kasih menu dulu ges
+
+print("1. Mode Personal 🎮")
+print("2. Mode Office 💼")
+print("3. Mode Public ☕")
+
+PilihanUser = input("Pilih opsi (1-3): ")
+if PilihanUser == "1":
+    a = "4:3"
+
+elif PilihanUser == "2":
+    a = "16:10"
+
+elif PilihanUser == "3":
+    a = "16:9"
+
+print(a)
