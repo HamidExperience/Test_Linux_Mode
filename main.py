470b82f71253e033e7389fd4eb71a44e8c095002
HamidExperience <Hamiddolanan@gmail.com>
2026-07-03 10:22:00 +0700

    Import subprocess & execute

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
+# Import library
+
+import subprocess
+
+# coba bikin perintahnya jadi gampang dulu
+
+def jalanin(P):
+    subprocess.run(P)
+
 # Kasih menu dulu ges
 
 print("1. Mode Personal 🎮")
 print("2. Mode Office 💼")
 print("3. Mode Public ☕")
 
+# Suruh user milih
+
 PilihanUser = input("Pilih opsi (1-3): ")
 if PilihanUser == "1":
-    a = "4:3"
+    a = ['whoami']
 
 elif PilihanUser == "2":
-    a = "16:10"
+    a = ['echo', 'ngetes', 'pilihan', 'kedua']
 
 elif PilihanUser == "3":
-    a = "16:9"
+    a = ['neofetch']
 
-print(a)
+# eksekusi
+
+jalanin(a)
