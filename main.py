6e563843e02e024fe62bd80d13780fdf673134e1
HamidExperience <Hamiddolanan@gmail.com>
2026-07-04 20:53:52 +0700

    Update menu and commands

--- main.py
+++ main.py
@@ -7,23 +7,26 @@ import subprocess
 def jalanin(P):
     subprocess.run(P)
 
+def menu():
+    print("1. Mode Personal 🎮")
+    print("2. Mode Office 💼")
+    print("3. Mode Public ☕")
+
 # Kasih menu dulu ges
 
-print("1. Mode Personal 🎮")
-print("2. Mode Office 💼")
-print("3. Mode Public ☕")
+menu()
 
 # Suruh user milih
 
 PilihanUser = input("Pilih opsi (1-3): ")
 if PilihanUser == "1":
-    a = ['whoami']
+    a = ['xrandr', '--output', 'eDP-1', '--mode', '1400x1050']
 
 elif PilihanUser == "2":
-    a = ['echo', 'ngetes', 'pilihan', 'kedua']
+    a = ['xrandr', '--output', 'eDP-1', '--mode', '1680x1050']
 
 elif PilihanUser == "3":
-    a = ['neofetch']
+    a = ['xrandr', '--output', 'eDP-1', '--mode', '1920x1080']
 
 # eksekusi
 
