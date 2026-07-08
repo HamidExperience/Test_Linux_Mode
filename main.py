d2d826f69b65a184976d3df49fdc747c5a94f155
HamidExperience <Hamiddolanan@gmail.com>
2026-07-08 12:03:15 +0700

    Add feature to change wallpaper and 2 personal mode options

--- main.py
+++ main.py
@@ -2,10 +2,53 @@
 
 import subprocess
 
-# coba bikin perintahnya jadi gampang dulu
+# Ini variable, yang ada '🥰️' itu bisa kalian ubah sesuai kebutuhan kalian
 
-def jalanin(P):
-    subprocess.run(P)
+# Variabel format-uri
+
+uri = "file://"
+
+# Ini lokasi Wallpaper 🥰️ tapi 'uri'nya jangan di hapus ya kalau memang tidak tau fungsinya
+WallpaperPersonal = uri + "/home/hamid/Pictures/Wallpaper/warmmountain.jpg"
+WallpaperOffice = uri + "/home/hamid/Pictures/Wallpaper/coldmountain.jpg"
+WallpaperPublic = uri + "/home/hamid/Pictures/Wallpaper/linuxmintwallpaperdark.png"
+
+# Ini untuk variable resolusi layar 🥰️
+Rasio4banding3 = "1400x1050"
+Rasio16banding10 = "1680x1050"
+Rasio16banding9 = "1920x1080"
+
+
+# Coba bikin perintahnya jadi gampang dulu
+
+def jalanin(p):
+    subprocess.run(p, check=True)
+
+def UbahWallpaper(w):
+    return ['gsettings', 'set', 'org.cinnamon.desktop.background', 'picture-uri', w]
+
+def UbahRasio(r):
+    return ['xrandr', '--output', 'eDP-1', '--mode', r]
+
+def ModePersonal():
+    print("1. Old (Ratio Aspect 4:3)\n2. New (Ratio Aspect 16:9)")
+    PilihanUser2 = input("Mau yang mana?: ")
+    if PilihanUser2 == "1":
+        jalanin(UbahRasio(Rasio4banding3))
+        jalanin(UbahWallpaper(WallpaperPersonal))
+    elif PilihanUser2 == "2":
+        jalanin(UbahRasio(Rasio16banding9))
+        jalanin(UbahWallpaper(WallpaperPersonal))
+    else:
+        print("Pilihan tidak valid!")
+
+def ModeOffice():
+    jalanin(UbahRasio(Rasio16banding10))
+    jalanin(UbahWallpaper(WallpaperOffice))
+
+def ModePublic():
+    jalanin(UbahRasio(Rasio16banding9))
+    jalanin(UbahWallpaper(WallpaperPublic))
 
 def menu():
     print("1. Mode Personal 🎮")
@@ -19,15 +62,17 @@ menu()
 # Suruh user milih
 
 PilihanUser = input("Pilih opsi (1-3): ")
+
+# Eksekusi sesuai pilihan
+
 if PilihanUser == "1":
-    a = ['xrandr', '--output', 'eDP-1', '--mode', '1400x1050']
+    ModePersonal()
 
 elif PilihanUser == "2":
-    a = ['xrandr', '--output', 'eDP-1', '--mode', '1680x1050']
+    ModeOffice()
 
 elif PilihanUser == "3":
-    a = ['xrandr', '--output', 'eDP-1', '--mode', '1920x1080']
+    ModePublic()
 
-# eksekusi
-
-jalanin(a)
+else:
+    print("\nPilihan tidak valid!\n")
